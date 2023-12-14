d7332dbaa65ceb621b212b86f4a383f60d20daee
Guilherme Meister Correa <88345985+gmeister18@users.noreply.github.com>
2023-12-14 13:43:54 -0300

    Update code

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php include_once 'includes/menu.inc.php' ?>
     <!-- FORMULARIO DE CADASTRO -->
     <div class="row container">
+      
         <p>&nbsp;</p>
         <form action="BD/create.php" method="POST" class="col s12">
           <fieldset class="formulario" style="padding: 20px">
