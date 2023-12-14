9f973578042b2384169e594effe84cb989cf540b
Guilherme Meister Correa <88345985+gmeister18@users.noreply.github.com>
2023-12-14 11:51:55 -0300

    Update code

--- busca.php
+++ busca.php
@@ -8,7 +8,6 @@
         <h5 class="light">Pesquisa de clientes e fornecedores</h5><hr>
         <p class="light">
         Você pesquisou por:
-        
             <?php 
                 include_once 'BD/conexao.php';
                 $busca = $_GET['pesquisa'];
